95c0a35e5a6825e956bb6ead5b5a3054380855d2
ikramalii <91293845+ikramalii@users.noreply.github.com>
2023-03-01 21:51:33 -0500

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -11,10 +11,19 @@ def index():
 def login():
     return render_template("login.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        birthday = request.form.get("birthday")
+        email = request.form.get("email")
+        selected_images = request.form.get("selected_images")
+        return "Congrats,You Succcesfully registered!!"
+    
     imageData = [{"image": "../static/images/image.png", "alt": "image1"}, 
                  {"image": "../static/images/image.png", "alt": "image2"}, 
                  {"image": "../static/images/image.png", "alt": "image3"}, 
                  {"image": "../static/images/image.png", "alt": "image4"}]
     return render_template("signup.html", imageData=imageData)
+
+
