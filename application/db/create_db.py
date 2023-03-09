90808975cc5ac7ba810f715ce12d7381a72f7a78
qas3 <123086564+qas3@users.noreply.github.com>
2023-03-09 05:51:09 -0500

    Add files via upload

---
+++ application/db/create_db.py
@@ -0,0 +1,35 @@
+import sqlite3 as db 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+UPLOADS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/images/')
+SQLPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "graphical_password.db")
+
+class DuplicateEmailError(Exception):
+    pass
+
+class DuplicateUsernameError(Exception):
+    pass
+
+def register_user(input_dict):
+
+    conn = db.connect(SQLPATH)
+
+    # Check if the email already exists in the database
+    cursor = conn.execute("SELECT EMAIL FROM USERS WHERE EMAIL=?", (input_dict['email'],))
+    row = cursor.fetchone()
+    if row is not None:
+        raise DuplicateEmailError('Email already exists in the database')
+
+    # Check if the username already exists in the database
+    cursor = conn.execute("SELECT USERNAME FROM USERS WHERE USERNAME=?", (input_dict['username'],))
+    row = cursor.fetchone()
+    if row is not None:
+        raise DuplicateUsernameError('Username already exists in the database')
+
+    # Insert the new user record into the database
+    conn.execute("INSERT INTO USERS (USERNAME, BIRTHDAY, EMAIL, PASSWORD_HASH) VALUES (?, ?, ?, ?)",
+                 (input_dict['username'], input_dict['birthday'], input_dict['email'], input_dict['password']))
+    conn.commit()
+    conn.close()
