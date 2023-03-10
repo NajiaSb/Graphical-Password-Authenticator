26c2d68c49bf49b239171ac96e0ec559ad9e61dd
ikramalii <91293845+ikramalii@users.noreply.github.com>
2023-03-10 18:31:34 -0500

    Update user_db.py

--- application/db/user_db.py
+++ application/db/user_db.py
@@ -11,23 +11,23 @@ class DuplicateEmailError(Exception):
 class DuplicateUsernameError(Exception):
     pass
 
+
 def register_user(input_dict):
 
     conn = db.connect(SQLPATH)
 
-    # Check if the email already exists in the database
+    # Checks if the email exists in the database
     cursor = conn.execute("SELECT EMAIL FROM USERS WHERE EMAIL=?", (input_dict['email'],))
     row = cursor.fetchone()
     if row is not None:
         raise DuplicateEmailError('Email already exists in the database')
 
-    # Check if the username already exists in the database
+    # Checks if the username exists in the database
     cursor = conn.execute("SELECT USERNAME FROM USERS WHERE USERNAME=?", (input_dict['username'],))
     row = cursor.fetchone()
     if row is not None:
         raise DuplicateUsernameError('Username already exists in the database')
 
-    # Insert the new user record into the database
     conn.execute("INSERT INTO USERS (USERNAME, BIRTHDAY, EMAIL, PASSWORD_HASH) VALUES (?, ?, ?, ?)",
                  (input_dict['username'], input_dict['birthday'], input_dict['email'], input_dict['password']))
     conn.commit()
